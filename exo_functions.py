d02dcdd237b279d00215b9d31f56f46fd81b63f1
Jesper <jesper@J.U.B.E.M>
2018-11-09 14:23:44 +0100

    Mange ændringer, blandt andet ny funktion der normerer

--- exo_functions.py
+++ exo_functions.py
@@ -9,9 +9,13 @@ import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
+# Get current time
+import datetime
+now = datetime.datetime.now()
+
 #%% Import function
 
-def import_tess_fits(path):
+def import_tess_fits(path,print_fig=False):
     hdulist = fits.open(path)
     hdu = hdulist[1]
     
@@ -22,18 +26,85 @@ def import_tess_fits(path):
     pdc_flux = []
     
     for i in range(len(hdu_data)):
-        time.append(hdu_data[i][0])
-        sap_flux.append(hdu_data[i][3])
-        pdc_flux.append(hdu_data[i][7])
+        if np.isnan(hdu_data[i][3])==False:
+            time.append(hdu_data[i][0])
+            sap_flux.append(hdu_data[i][3])
+            pdc_flux.append(hdu_data[i][7])
         
     time =      np.array(time)
     sap_flux =  np.array(sap_flux)
     pdc_flux =  np.array(pdc_flux)
-        
-    plt.figure()
-    plt.plot(time, sap_flux)
-    plt.title(path)
-    plt.xlabel('time [days]')
-    plt.ylabel('flux')
+    
+    if print_fig==True:    
+        plt.figure()
+        plt.plot(time, sap_flux, ',')
+        plt.title(path)
+        plt.xlabel('time [days]')
+        plt.ylabel('flux')
     
     return time, sap_flux
+
+#%%
+def normer_fluxes(times,fluxes,intervals,print_fig=False,save_fig=False):
+    med_fluxes = []
+    for i in range(len(fluxes)):
+        med_flux = []
+        for j in range(len(fluxes[i])-2*intervals[i]):
+            med_flux.append(np.median(fluxes[i][j:j+2*intervals[i]]))
+        med_flux = np.array(med_flux)    
+        med_fluxes.append(med_flux)
+    
+    med_fluxes=np.array(med_fluxes)
+    
+    norm_fluxes = []
+    norm_times = []
+    for i in range(len(fluxes)):
+        norm_flux = fluxes[i][intervals[i]:-intervals[i]]/med_fluxes[i]
+        norm_fluxes.append(norm_flux)
+        norm_time = times[i][intervals[i]:-intervals[i]]
+        norm_times.append(norm_time)
+    norm_fluxes = np.array(norm_fluxes)
+    norm_times = np.array(norm_times)
+        
+    if print_fig==True:        
+        for i in range(len(norm_fluxes)):
+            plt.figure()
+            plt.subplot(2,1,1)
+            plt.plot(norm_times[i], fluxes[i][intervals[i]:-intervals[i]], 'b,')
+            plt.plot(norm_times[i], med_fluxes[i], 'r-')
+            plt.xlabel('Time [days]')
+            plt.ylabel('Flux')
+            plt.axis(xmin=min(norm_times[i]),
+                     xmax=max(norm_times[i]),
+                     ymin=np.median(fluxes[i])-300,
+                     ymax=np.median(fluxes[i])+300
+                     )
+            plt.subplot(2,1,2)
+            plt.plot(norm_times[i],norm_fluxes[i],'k,')
+            plt.xlabel('Time [days]')
+            plt.ylabel('Normalized Flux')
+            plt.axis(xmin=min(norm_times[i]),
+                     xmax=max(norm_times[i]),
+                     ymin=0.98,
+                     ymax=1.02
+                     )            
+            plt.tight_layout()
+            plt.show()
+            if save_fig==True:
+                plt.savefig('figures/' + 
+                            str(now.year) + 
+                            '-' + 
+                            str(now.month) + 
+                            '-' + 
+                            str(now.day) +
+                            '_' +
+                            str(now.hour) +
+                            ':' +
+                            str(now.minute) +
+                            ':' +
+                            str(now.second) +
+                            '_norm_curve' +
+                            str(i) +
+                            '.pdf')
+
+    return norm_times, norm_fluxes
--- exo_main.py
+++ exo_main.py
@@ -12,6 +12,18 @@ import exo_functions as ex
 plt.close('all')
 
 paths = np.genfromtxt('fits.txt', dtype=str)
+
+times =     []
+fluxes =    [] 
                  
 for i in range(len(paths)):
-    ex.import_tess_fits(paths[i])
+    time, flux = ex.import_tess_fits(paths[i], print_fig=False)
+    times.append(time)
+    fluxes.append(flux)
+
+times = np.array(times)
+fluxes = np.array(fluxes)
+
+intervals = [500,250,150]
+
+norm_times, norm_fluxes = ex.normer_fluxes(times,fluxes,intervals,print_fig=True, save_fig=True)
